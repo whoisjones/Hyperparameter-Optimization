daa53b7b787c1675960b1d10205d4655365316a9
whoisjones <jonas.golde@gmail.com>
2020-09-18 12:35:25 +0200

    testing ready for genetic text classification

--- FlairParamOptimizer/search_strategies.py
+++ FlairParamOptimizer/search_strategies.py
@@ -41,9 +41,9 @@ class EvolutionarySearch(SearchStrategy):
 
     def __init__(
             self,
-            population_size: int = 8,
+            population_size: int = 12,
             cross_rate: float = 0.4,
-            mutation_rate: float = 0.01,
+            mutation_rate: float = 0.05,
     ):
         super().__init__()
         self.population_size = population_size
@@ -60,15 +60,28 @@ class EvolutionarySearch(SearchStrategy):
         else:
             return False
 
-    def _evolve(self, search_space: SearchSpace, current_results: dict):
-        log.info("Start evolution")
+    def _evolve(self, search_space: SearchSpace, results: dict):
+        current_results = self._get_current_results(results)
+        log.info(50*'-')
+        log.info("Starting evolution - this generation had following results:")
+        for idx, result in enumerate(current_results.values()):
+            log.info(f"Configuration {idx} with a result of {result.get('result')}.")
         parent_population = self._get_parent_population(current_results)
         selected_population = self._select(current_results)
-        for child in selected_population:
+        for idx, child in enumerate(selected_population):
+            log.info(50 * '-')
+            log.info(f"Performing crossover and mutation for configuration {idx}.")
             child = self._crossover(child, parent_population)
             child = self._mutate(child, search_space.parameter_storage)
             search_space.training_configurations._add_configuration(child)
         log.info("Evolution completed.")
+
+    def _get_current_results(self, results:dict):
+        most_recent_ids = np.arange(0, len(results))[-self.population_size:]
+        key_generator = lambda id: f"training-run-{id}"
+        current_keys = list(map(key_generator, most_recent_ids))
+        current_results = {key: value for key, value in results.items() if key in current_keys}
+        return current_results
 
     def _get_parent_population(self, results: dict) -> dict:
         parent_population = self._extract_configurations_from_results(results)
@@ -89,8 +102,7 @@ class EvolutionarySearch(SearchStrategy):
             if embedding_key in grouped_parent_population:
                 grouped_parent_population[embedding_key].append(embedding_value)
             else:
-                #TODO fix list / dict bug
-                grouped_parent_population[embedding_key] = embedding_value
+                grouped_parent_population[embedding_key] = [embedding_value]
         return grouped_parent_population
 
     def _get_embedding_key(self, embedding: dict):
@@ -103,6 +115,9 @@ class EvolutionarySearch(SearchStrategy):
     def _select(self, current_results: dict) -> np.array:
         current_configurations = [result.get("params") for result in current_results.values()]
         evolution_probabilities = self._get_fitness(current_results)
+        log.info(50 * '-')
+        for idx, prob in enumerate(evolution_probabilities):
+            log.info(f"The evolution probability for configuration {idx} is: {prob}.")
         return np.random.choice(current_configurations, size=self.population_size, replace=True, p=evolution_probabilities)
 
     def _get_fitness(self, results: dict):
@@ -116,19 +131,27 @@ class EvolutionarySearch(SearchStrategy):
         DNA_size = len(child)
         if np.random.rand() < self.cross_rate:
             random_configuration = randrange(configuration_with_same_embedding)  # select another individual from pop
-            parent = parent_population.get(child_type)[random_configuration]
+            parent = {**parent_population.get(child_type)[random_configuration]}
             cross_points = np.random.randint(0, 2, DNA_size).astype(np.bool)  # choose crossover points
             for (parameter, value), replace in zip(child.items(), cross_points):
                 if replace:
                     child[parameter] = parent[parameter]  # mating and produce one child
+            log.info(f"Crossover performed for current for configuration ({np.sum(cross_points)} parameters changed).")
+        else:
+            log.info("No crossover for current configuration.")
         return child
 
     def _mutate(self, child: dict, parameter_storage: ParameterStorage):
         child_type = self._get_embedding_key(child)
-        for parameter in child.keys():
-            if np.random.rand() < self.mutation_rate:
+        mutation_points = np.where(np.random.rand(len(child)) < self.mutation_rate, 1, 0).astype(bool)
+        for parameter, replace in zip(child.keys(), mutation_points):
+            if replace:
                 if parameter in getattr(parameter_storage, child_type):
-                    child[parameter] = random.sample(getattr(parameter_storage, child_type).get(parameter), 1)
+                    child[parameter] = self._sample_parameter(child_type, parameter, parameter_storage)
                 elif parameter in getattr(parameter_storage, "GeneralParameters"):
-                    child[parameter] = random.sample(getattr(parameter_storage, "GeneralParameters").get(parameter), 1)
+                    child[parameter] = self._sample_parameter("GeneralParameters", parameter, parameter_storage)
+        log.info(f"{np.sum(mutation_points)} parameters have been mutated.")
         return child
+
+    def _sample_parameter(self, key: str, parameter: str, parameter_storage: ParameterStorage):
+        return random.sample(getattr(parameter_storage, key).get(parameter), 1).pop()
--- evaluation_files/evaluation_senteval-sst_genetic.py
+++ evaluation_files/evaluation_senteval-sst_genetic.py
@@ -1,20 +1,20 @@
 from FlairParamOptimizer import search_strategies, search_spaces, orchestrator
 import FlairParamOptimizer.parameter_listings.parameters_for_user_input as param
-from flair.datasets import TREC_6
+from flair.datasets import SENTEVAL_SST_GRANULAR
 from torch.optim import SGD, Adam
 
 # 1.) Define your corpus
-corpus = TREC_6()
+corpus = SENTEVAL_SST_GRANULAR()
 
 # 2.) create an search space
 search_space = search_spaces.TextClassifierSearchSpace()
-search_strategy = search_strategies.EvolutionarySearch(population_size=4, mutation_rate=1, cross_rate=1)
+search_strategy = search_strategies.EvolutionarySearch()
 
 # 3.) depending on your task add the respective parameters you want to optimize over
-search_space.add_budget(param.Budget.GENERATIONS, 3)
+search_space.add_budget(param.Budget.TIME_IN_H, 24)
 search_space.add_evaluation_metric(param.EvaluationMetric.MICRO_F1_SCORE)
 search_space.add_optimization_value(param.OptimizationValue.DEV_SCORE)
-search_space.add_max_epochs_per_training_run(1)
+search_space.add_max_epochs_per_training_run(15)
 
 #Depending on your downstream task, add embeddings and specify these with the respective Parameters below
 search_space.add_parameter(param.ModelTrainer.LEARNING_RATE, options=[0.01, 0.05, 0.1])
@@ -34,13 +34,13 @@ search_space.add_parameter(param.DocumentPoolEmbeddings.WORD_EMBEDDINGS, options
 search_space.add_parameter(param.DocumentPoolEmbeddings.POOLING, options=['mean', 'max', 'min'])
 
 #Define parameters for Transformers
-#search_space.add_parameter(param.TransformerDocumentEmbeddings.MODEL, sampling_func.choice, options=["bert-base-uncased", "distilbert-base-uncased"])
-#search_space.add_parameter(param.TransformerDocumentEmbeddings.BATCH_SIZE, sampling_func.choice, options=[16, 32, 64])
+search_space.add_parameter(param.TransformerDocumentEmbeddings.MODEL, options=["bert-base-uncased", "distilbert-base-uncased"])
+search_space.add_parameter(param.TransformerDocumentEmbeddings.BATCH_SIZE, options=[16, 32, 64])
 
 search_strategy.make_configurations(search_space)
 
 orchestrator = orchestrator.Orchestrator(corpus=corpus,
-                                         base_path='resources/evaluation-trec-grid-DRAFT',
+                                         base_path='resources/evaluation-senteval-sst-genetic',
                                          search_space=search_space,
                                          search_strategy=search_strategy)
 
