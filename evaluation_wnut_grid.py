a14e77f5e89f33b7de8291fdc2f614b434b470d9
whoisjones <jonas.golde@gmail.com>
2020-09-21 10:17:07 +0200

    sequence labeling evaluation

--- evaluation_wnut_grid.py
+++ evaluation_wnut_grid.py
@@ -5,7 +5,7 @@ from flair.embeddings import WordEmbeddings
 from flair.data import Corpus
 from flair.datasets import WNUT_17
 
-corpus = WNUT_17().downsample(0.05)
+corpus = WNUT_17()
 
 search_space = search_spaces.SequenceTaggerSearchSpace()
 search_strategy = search_strategies.GridSearch()
@@ -15,7 +15,7 @@ search_space.add_tag_type("ner")
 search_space.add_budget(param.Budget.TIME_IN_H, 24)
 search_space.add_evaluation_metric(param.EvaluationMetric.MICRO_F1_SCORE)
 search_space.add_optimization_value(param.OptimizationValue.DEV_SCORE)
-search_space.add_max_epochs_per_training_run(1)
+search_space.add_max_epochs_per_training_run(5)
 
 search_space.add_parameter(param.SequenceTagger.HIDDEN_SIZE, options=[128, 256, 512])
 search_space.add_parameter(param.SequenceTagger.DROPOUT, options=[0, 0.5])
