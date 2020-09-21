42c9316b31c262849c168a6a7cff2b3e121f7fb1
whoisjones <jonas.golde@gmail.com>
2020-09-21 12:33:30 +0200

    tests added for search strategy

--- tests/test_search_space.py
+++ tests/test_search_space.py
@@ -1,59 +1,75 @@
 import pytest
 from FlairParamOptimizer import search_spaces, search_strategies
-import FlairParamOptimizer.parameter_listings.parameters_for_user_input as params
+import FlairParamOptimizer.parameter_listings.parameters_for_user_input as param
+
+def build_correct_setup():
+    search_space = search_spaces.TextClassifierSearchSpace()
+    search_strategy = search_strategies.RandomSearch()
+    search_space.add_budget(param.Budget.GENERATIONS, 10)
+    search_space.add_evaluation_metric(param.EvaluationMetric.MICRO_F1_SCORE)
+    search_space.add_optimization_value(param.OptimizationValue.DEV_SCORE)
+    search_space.add_parameter(param.ModelTrainer.LEARNING_RATE, options=[0.01, 0.05, 0.1])
+    search_space.add_parameter(param.DocumentRNNEmbeddings.HIDDEN_SIZE, options=[128, 256, 512])
+    search_space.add_parameter(param.DocumentRNNEmbeddings.WORD_EMBEDDINGS, options=[['glove'], ['en'], ['en', 'glove']])
+    return search_space, search_strategy
 
 def test_parameters_are_missing():
     search_space = search_spaces.TextClassifierSearchSpace()
     search_strategy = search_strategies.RandomSearch()
-    search_space.add_optimization_value(params.OptimizationValue.DEV_SCORE)
-    search_space.add_evaluation_metric(params.EvaluationMetric.MICRO_F1_SCORE)
-    search_space.add_budget(params.Budget.RUNS, 5)
+    search_space.add_optimization_value(param.OptimizationValue.DEV_SCORE)
+    search_space.add_evaluation_metric(param.EvaluationMetric.MICRO_F1_SCORE)
+    search_space.add_budget(param.Budget.RUNS, 5)
     with pytest.raises(AttributeError):
         search_space.check_completeness(search_strategy.search_strategy_name)
 
 def test_budget_is_missing():
     search_space = search_spaces.TextClassifierSearchSpace()
     search_strategy = search_strategies.RandomSearch()
-    search_space.add_optimization_value(params.OptimizationValue.DEV_SCORE)
-    search_space.add_evaluation_metric(params.EvaluationMetric.MICRO_F1_SCORE)
+    search_space.add_optimization_value(param.OptimizationValue.DEV_SCORE)
+    search_space.add_evaluation_metric(param.EvaluationMetric.MICRO_F1_SCORE)
     with pytest.raises(AttributeError):
         search_space.check_completeness(search_strategy.search_strategy_name)
 
 def test_optimization_value_is_missing():
     search_space = search_spaces.TextClassifierSearchSpace()
     search_strategy = search_strategies.RandomSearch()
-    search_space.add_optimization_value(params.OptimizationValue.DEV_SCORE)
-    search_space.add_budget(params.Budget.RUNS, 10)
+    search_space.add_optimization_value(param.OptimizationValue.DEV_SCORE)
+    search_space.add_budget(param.Budget.RUNS, 10)
     with pytest.raises(AttributeError):
         search_space.check_completeness(search_strategy.search_strategy_name)
 
 def test_evaluation_metric_is_missing():
     search_space = search_spaces.TextClassifierSearchSpace()
     search_strategy = search_strategies.RandomSearch()
-    search_space.add_budget(params.Budget.RUNS, 10)
-    search_space.add_evaluation_metric(params.EvaluationMetric.MICRO_F1_SCORE)
+    search_space.add_budget(param.Budget.RUNS, 10)
+    search_space.add_evaluation_metric(param.EvaluationMetric.MICRO_F1_SCORE)
     with pytest.raises(AttributeError):
         search_space.check_completeness(search_strategy.search_strategy_name)
 
 def test_word_embeddings_are_missing():
     search_space = search_spaces.TextClassifierSearchSpace()
     search_strategy = search_strategies.RandomSearch()
-    search_space.add_budget(params.Budget.GENERATIONS, 10)
-    search_space.add_evaluation_metric(params.EvaluationMetric.MICRO_F1_SCORE)
-    search_space.add_optimization_value(params.OptimizationValue.DEV_SCORE)
-    search_space.add_parameter(params.ModelTrainer.LEARNING_RATE, options=[0.01, 0.05, 0.1])
-    search_space.add_parameter(params.DocumentRNNEmbeddings.HIDDEN_SIZE, options=[128, 256, 512])
+    search_space.add_budget(param.Budget.GENERATIONS, 10)
+    search_space.add_evaluation_metric(param.EvaluationMetric.MICRO_F1_SCORE)
+    search_space.add_optimization_value(param.OptimizationValue.DEV_SCORE)
+    search_space.add_parameter(param.ModelTrainer.LEARNING_RATE, options=[0.01, 0.05, 0.1])
+    search_space.add_parameter(param.DocumentRNNEmbeddings.HIDDEN_SIZE, options=[128, 256, 512])
     with pytest.raises(KeyError):
         search_space.check_completeness(search_strategy.search_strategy_name)
 
 def test_budget_type_switch_if_search_strategy_does_not_match():
     search_space = search_spaces.TextClassifierSearchSpace()
     search_strategy = search_strategies.RandomSearch()
-    search_space.add_budget(params.Budget.GENERATIONS, 10)
-    search_space.add_evaluation_metric(params.EvaluationMetric.MICRO_F1_SCORE)
-    search_space.add_optimization_value(params.OptimizationValue.DEV_SCORE)
-    search_space.add_parameter(params.ModelTrainer.LEARNING_RATE, options=[0.01, 0.05, 0.1])
-    search_space.add_parameter(params.DocumentRNNEmbeddings.HIDDEN_SIZE, options=[128, 256, 512])
-    search_space.add_parameter(params.DocumentRNNEmbeddings.WORD_EMBEDDINGS, options=[['glove'], ['en'], ['en', 'glove']])
+    search_space.add_budget(param.Budget.GENERATIONS, 10)
+    search_space.add_evaluation_metric(param.EvaluationMetric.MICRO_F1_SCORE)
+    search_space.add_optimization_value(param.OptimizationValue.DEV_SCORE)
+    search_space.add_parameter(param.ModelTrainer.LEARNING_RATE, options=[0.01, 0.05, 0.1])
+    search_space.add_parameter(param.DocumentRNNEmbeddings.HIDDEN_SIZE, options=[128, 256, 512])
+    search_space.add_parameter(param.DocumentRNNEmbeddings.WORD_EMBEDDINGS, options=[['glove'], ['en'], ['en', 'glove']])
     search_space.check_completeness(search_strategy.search_strategy_name)
-    assert search_space.budget.budget_type == params.Budget.RUNS.value
+    assert search_space.budget.budget_type == param.Budget.RUNS.value
+
+def test_parameter_storage():
+    search_space, search_strategy = build_correct_setup()
+    assert hasattr(search_space.parameter_storage, "DocumentRNNEmbeddings")
+    assert hasattr(search_space.parameter_storage, "GeneralParameters")
---
+++ tests/test_search_strategy.py
@@ -0,0 +1,46 @@
+import pytest
+import collections
+from FlairParamOptimizer import search_spaces, search_strategies
+import FlairParamOptimizer.parameter_listings.parameters_for_user_input as param
+
+def build_correct_setup():
+    search_space = search_spaces.TextClassifierSearchSpace()
+    search_strategy = search_strategies.RandomSearch()
+    search_space.add_budget(param.Budget.GENERATIONS, 10)
+    search_space.add_evaluation_metric(param.EvaluationMetric.MICRO_F1_SCORE)
+    search_space.add_optimization_value(param.OptimizationValue.DEV_SCORE)
+    search_space.add_parameter(param.ModelTrainer.LEARNING_RATE, options=[0.01, 0.05, 0.1])
+    search_space.add_parameter(param.DocumentRNNEmbeddings.HIDDEN_SIZE, options=[128, 256, 512])
+    search_space.add_parameter(param.DocumentRNNEmbeddings.WORD_EMBEDDINGS, options=[['glove'], ['en'], ['en', 'glove']])
+    return search_space, search_strategy
+
+def build_correct_evolutionary_setup():
+    search_space = search_spaces.TextClassifierSearchSpace()
+    search_strategy = search_strategies.EvolutionarySearch()
+    search_space.add_budget(param.Budget.GENERATIONS, 10)
+    search_space.add_evaluation_metric(param.EvaluationMetric.MICRO_F1_SCORE)
+    search_space.add_optimization_value(param.OptimizationValue.DEV_SCORE)
+    search_space.add_parameter(param.ModelTrainer.LEARNING_RATE, options=[0.01, 0.05, 0.1])
+
+    search_space.add_parameter(param.DocumentRNNEmbeddings.HIDDEN_SIZE, options=[128, 256, 512])
+    search_space.add_parameter(param.DocumentRNNEmbeddings.WORD_EMBEDDINGS,
+                               options=[['glove'], ['en'], ['en', 'glove']])
+
+    search_space.add_parameter(param.DocumentPoolEmbeddings.WORD_EMBEDDINGS,
+                               options=[['glove'], ['en'], ['en', 'glove']])
+    search_space.add_parameter(param.DocumentPoolEmbeddings.POOLING, options=['mean', 'max', 'min'])
+    return search_space, search_strategy
+
+def test_type_of_make_configurations():
+    search_space, search_strategy = build_correct_setup()
+    search_strategy.make_configurations(search_space)
+    assert type(search_space.training_configurations.configurations) == list
+    assert len(search_space.training_configurations.configurations) == 27
+
+def test_evolutionary_configuration():
+    search_space, search_strategy = build_correct_evolutionary_setup()
+    search_strategy.make_configurations(search_space)
+    assert len(search_space.training_configurations.configurations) == 12
+    list_of_embeddings_from_configurations = [item.get("document_embeddings").__name__ for item in search_space.training_configurations.configurations]
+    occurrences = collections.Counter(list_of_embeddings_from_configurations)
+    assert list(occurrences.values()) == [6,6]
